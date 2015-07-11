f97e7d718b96a90a04ecd3cd1bccdf718619bf05
Nathaniel Parrish <parrish.nate@gmail.com>
2015-07-11 12:44:53 -0700

    implemented collision detection, shuriken images, and score board

--- watchout.js
+++ watchout.js
@@ -12,33 +12,41 @@ var drag = d3.behavior.drag()
 
   })
   .on("drag", function(){
-    d3.select(this).attr("cx", d3.event.x).attr("cy", d3.event.y);
+    d3.select(this).attr("cx", function() {
+      if(d3.event.x < 10) {
+        return 10;
+      } else if(d3.event.x > 490) {
+        return 490;
+      } else {
+        return d3.event.x
+      }
+    }).attr("cy", function() {
+      if(d3.event.y < 10) {
+        return 10;
+      } else if(d3.event.y > 490) {
+        return 490;
+      } else {
+        return d3.event.y;
+      }
+    });
   })
   .on("dragend", function(){
 
   });
 
 
-
-
-// function click(){
-//   // Ignore the click event if it was suppressed
-//   if (d3.event.defaultPrevented) return;
-
-//   // Extract the click location\    
-//   var point = d3.mouse(this)
-//   , p = {x: point[0], y: point[1] };
-// }
-
-// function dragmove(d) {
-//   var x = d3.event.x;
-//   var y = d3.event.y;
-//   d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
-// }
-
 var initialize = function(){
   //add svg
-  var svg = d3.select("body").append("svg").attr("height", "500").attr("width", "500");
+  d3.select("body").append("svg").attr("height", "500").attr("width", "500")
+    .append("defs")
+    .append("pattern")
+    .attr("id", "bg")
+    .attr("height", 20)
+    .attr("width", 20)
+    .append("image")
+    .attr("xlink:href", "shuriken.png").attr('width', 20).attr('height', 20);
+  d3.select('svg').append('rect').attr('height', 500).attr('width', 500).attr('fill', 'red').style('display', 'none');
+
 };
 
 
@@ -46,26 +54,61 @@ var initialize = function(){
 var insertPlayers = function(){
   for (var i = 0; i < 10; i++){
     d3.select("svg").append("circle").attr("class", "enemy").attr("cx", Math.random() * 500)
-    .attr("cy", Math.random() * 500).attr("r", 10).attr("fill", "black");
+      .attr("cy", Math.random() * 500).attr("r", 10).attr('fill', 'url(#bg)');
+      /*.append('defs')
+      .append('pattern').attr('id', 'img1').attr('patternUnits', 'userSpaceOnUse')
+        .attr('width', 20).attr('height', 20)
+        .append('image').attr('xlink:href', 'shuriken.png').attr('x', 0).attr('y', 0)*/
+
+      // <defs>
+      //   <pattern id="img1" patternUnits="userSpaceOnUse" width="100" height="100">
+      //   <image xlink:href="shuriken.png" x="0" y="0" width="20" height="20" />
+      //   </pattern>
+      // </defs>
+
   }
   var hero = d3.select("svg").append("circle").attr("class", "hero").attr("cx", 250)
     .attr("cy", 250).attr("r", 10).attr("fill", "blue").call(drag);
-  //var drag = d3.behavior.drag().on("drag", dragmove);
-  // hero.selectAll(".hero").call(dragmove);
 };
 
-    // var text = svg.selectAll("text")
-  //     .data(data, function(d) { return d; });
-
-  // text.attr("x", function(d, i) { return i * 32; })
 var updateEnemies = function(){
   d3.select("svg").selectAll(".enemy").transition().duration(1000).attr("cx", function(){return Math.random() * 500})
     .attr("cy", function(){return Math.random() * 500});
 }
 
-// var drag = d3.behavior.drag();
-// selection.call(drag);
+var collisionCheck = function(){
+  var enemy = d3.selectAll('.enemy')[0];
+  var hero = d3.selectAll('.hero')[0][0];
+  for (var i = 0; i < 10; i++){
+    if (Math.abs(enemy[i].cx.animVal.value - hero.cx.animVal.value) <= 20 &&
+        Math.abs(enemy[i].cy.animVal.value - hero.cy.animVal.value) <= 20 ){
+      if(currentScore > highScore) {
+        highScore = currentScore;
+        d3.select('.high').text(highScore);
+      }
+      currentScore = 0;
+      collisions++;
+      d3.select('.collisions').text(collisions);
+      d3.select('rect').style('display', 'inline');
+      setTimeout(function(){d3.select('rect').style('display', 'none')}, 20);
+    }
+  }
+};
+
+var score = function(){
+  d3.select('.score').text(++currentScore);
+}
+
+
+
+var enemies = [];d3.selectAll(".enemy");
+var currentScore = 0;
+var highScore = 0;
+var collisions = 0;
+
 
 initialize();
 insertPlayers();
 setInterval(updateEnemies, 1000);
+setInterval(collisionCheck, 1);
+setInterval(score, 100);
